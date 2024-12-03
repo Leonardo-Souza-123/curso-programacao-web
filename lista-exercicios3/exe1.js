62801f6c416babb1a1a22ffc05f917c4a82e8c24
Leonardo <leubr828@gmail.com>
2024-12-03 20:33:17 -0300

    init repo

--- lista-exercicios3/exe1.js
+++
@@ -1,8 +0,0 @@
-document.querySelector("#formulario").addEventListener("submit", function(evento){
-    evento.preventDefault()
-    var raio = document.querySelector("#raio").value
-    var area = 2 * Math.PI * Number(raio)
-    console.log(area)
-
-    document.querySelector("#resultado").innerHTML = `a area é ${area.toFixed(2)}`
-})
